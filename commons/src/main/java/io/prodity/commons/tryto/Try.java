f439d9e1c2d1be437b5caf5890a41fb371ad6f3f
FakeNeth <cneth97@gmail.com>
2018-05-02 14:22:04 +0300

    @Nonnull annotations & Preconditions

--- commons/src/main/java/io/prodity/commons/tryto/Try.java
+++ commons/src/main/java/io/prodity/commons/tryto/Try.java
@@ -1,17 +1,20 @@
 package io.prodity.commons.tryto;
 
+import com.google.common.base.Preconditions;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import javax.annotation.Nonnull;
 
 public enum Try {
 
     ;
 
-    public static Runnable to(CheckedRunnable runnable) {
+    public static Runnable to(@Nonnull CheckedRunnable runnable) {
+        Preconditions.checkNotNull(runnable, "runnable");
         return () -> {
             try {
                 runnable.run();
@@ -21,15 +24,16 @@ public enum Try {
         };
     }
 
-    public static Runnable runnable(CheckedRunnable runnable) {
+    public static Runnable runnable(@Nonnull CheckedRunnable runnable) {
         return Try.to(runnable);
     }
 
-    public static void run(CheckedRunnable runnable) {
+    public static void run(@Nonnull CheckedRunnable runnable) {
         Try.to(runnable).run();
     }
 
-    public static <T> Supplier<T> to(CheckedSupplier<T> supplier) {
+    public static <T> Supplier<T> to(@Nonnull CheckedSupplier<T> supplier) {
+        Preconditions.checkNotNull(supplier, "supplier");
         return () -> {
             try {
                 return supplier.get();
@@ -39,15 +43,16 @@ public enum Try {
         };
     }
 
-    public static <T> Supplier<T> supplier(CheckedSupplier<T> supplier) {
+    public static <T> Supplier<T> supplier(@Nonnull CheckedSupplier<T> supplier) {
         return Try.to(supplier);
     }
 
-    public static <T> T get(CheckedSupplier<T> supplier) {
+    public static <T> T get(@Nonnull CheckedSupplier<T> supplier) {
         return Try.to(supplier).get();
     }
 
-    public static <T, R> Function<T, R> to(CheckedFunction<T, R> function) {
+    public static <T, R> Function<T, R> to(@Nonnull CheckedFunction<T, R> function) {
+        Preconditions.checkNotNull(function, "function");
         return (value) -> {
             try {
                 return function.apply(value);
@@ -57,11 +62,12 @@ public enum Try {
         };
     }
 
-    public static <T, R> Function<T, R> function(CheckedFunction<T, R> function) {
+    public static <T, R> Function<T, R> function(@Nonnull CheckedFunction<T, R> function) {
         return Try.to(function);
     }
 
-    public static <T1, T2, R> BiFunction<T1, T2, R> to(CheckedBiFunction<T1, T2, R> biFunction) {
+    public static <T1, T2, R> BiFunction<T1, T2, R> to(@Nonnull CheckedBiFunction<T1, T2, R> biFunction) {
+        Preconditions.checkNotNull(biFunction, "biFunction");
         return (value1, value2) -> {
             try {
                 return biFunction.apply(value1, value2);
@@ -71,11 +77,12 @@ public enum Try {
         };
     }
 
-    public static <T1, T2, R> BiFunction<T1, T2, R> biFunction(CheckedBiFunction<T1, T2, R> biFunction) {
+    public static <T1, T2, R> BiFunction<T1, T2, R> biFunction(@Nonnull CheckedBiFunction<T1, T2, R> biFunction) {
         return Try.to(biFunction);
     }
 
-    public static <T> Consumer<T> to(CheckedConsumer<T> consumer) {
+    public static <T> Consumer<T> to(@Nonnull CheckedConsumer<T> consumer) {
+        Preconditions.checkNotNull(consumer, "consumer");
         return (value) -> {
             try {
                 consumer.accept(value);
@@ -85,11 +92,12 @@ public enum Try {
         };
     }
 
-    public static <T> Consumer<T> consumer(CheckedConsumer<T> consumer) {
+    public static <T> Consumer<T> consumer(@Nonnull CheckedConsumer<T> consumer) {
         return Try.to(consumer);
     }
 
-    public static <T1, T2> BiConsumer<T1, T2> to(CheckedBiConsumer<T1, T2> biConsumer) {
+    public static <T1, T2> BiConsumer<T1, T2> to(@Nonnull CheckedBiConsumer<T1, T2> biConsumer) {
+        Preconditions.checkNotNull(biConsumer, "biConsumer");
         return (value1, value2) -> {
             try {
                 biConsumer.accept(value1, value2);
@@ -99,11 +107,12 @@ public enum Try {
         };
     }
 
-    public static <T1, T2> BiConsumer<T1, T2> biConsumer(CheckedBiConsumer<T1, T2> biConsumer) {
+    public static <T1, T2> BiConsumer<T1, T2> biConsumer(@Nonnull CheckedBiConsumer<T1, T2> biConsumer) {
         return Try.to(biConsumer);
     }
 
-    public static <T> Predicate<T> to(CheckedPredicate<T> predicate) {
+    public static <T> Predicate<T> to(@Nonnull CheckedPredicate<T> predicate) {
+        Preconditions.checkNotNull(predicate, "predicate");
         return (value) -> {
             try {
                 return predicate.test(value);
@@ -113,16 +122,12 @@ public enum Try {
         };
     }
 
-    public static <T> Predicate<T> predicate(CheckedPredicate<T> predicate) {
+    public static <T> Predicate<T> predicate(@Nonnull CheckedPredicate<T> predicate) {
         return Try.to(predicate);
     }
 
-    public static <T> Predicate<T> toAndNegate(CheckedPredicate<T> predicate) {
+    public static <T> Predicate<T> toAndNegate(@Nonnull CheckedPredicate<T> predicate) {
         return Try.to(predicate.negate());
-    }
-
-    public static <T> Predicate<T> negate(Predicate<T> predicate) {
-        return predicate.negate();
     }
 
     private static RuntimeException propagate(Throwable throwable) {
