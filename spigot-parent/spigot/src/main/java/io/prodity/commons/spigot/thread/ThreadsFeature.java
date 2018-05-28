7396081c5f5cc7222b4ed946ccc0366f54c797eb
FakeNeth <cneth97@gmail.com>
2018-05-28 08:32:21 +0300

    @Service to ThreadsFeature

--- spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/thread/ThreadsFeature.java
+++ spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/thread/ThreadsFeature.java
@@ -2,7 +2,9 @@ package io.prodity.commons.spigot.thread;
 
 import io.prodity.commons.inject.InjectionFeature;
 import io.prodity.commons.plugin.ProdityPlugin;
+import org.jvnet.hk2.annotations.Service;
 
+@Service
 public class ThreadsFeature implements InjectionFeature {
 
     @Override
