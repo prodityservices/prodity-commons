392b853c1b64f8e1863c30466ac54d7ded10e396
FakeNeth <cneth97@gmail.com>
2018-06-11 02:27:07 +0300

    anvil 1_8_R3 impl

---
+++ spigot-parent/v1_8_R3/src/main/java/io/prodity/commons/spigot/legacy/v1_8_R3/AnvilGuiInventoryImpl.java
@@ -0,0 +1,61 @@
+package io.prodity.commons.spigot.legacy.v1_8_R3;
+
+import io.prodity.commons.spigot.legacy.gui.anvil.AnvilGuiInventory;
+import io.prodity.commons.spigot.legacy.gui.anvil.AnvilInputListener;
+import javax.annotation.Nullable;
+import net.minecraft.server.v1_8_R3.ChatMessage;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import net.minecraft.server.v1_8_R3.PacketPlayOutOpenWindow;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftInventoryAnvil;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class AnvilGuiInventoryImpl extends CraftInventoryAnvil implements AnvilGuiInventory {
+
+    private final ContainerAnvilImpl anvil;
+
+    public AnvilGuiInventoryImpl(ContainerAnvilImpl anvil) {
+        super(anvil.craftInventory, anvil.resultInventory);
+        this.anvil = anvil;
+    }
+
+    @Override
+    public void addListener(AnvilInputListener listener) {
+        this.anvil.listeners.add(listener);
+    }
+
+    @Override
+    public void removeListener(AnvilInputListener listener) {
+        this.anvil.listeners.remove(listener);
+    }
+
+    @Override
+    public String getCurrentText() {
+        return this.anvil.currentName;
+    }
+
+    @Override
+    public void setCurrentText(@Nullable String text) {
+        this.anvil.currentName = text;
+        final ItemStack item = this.getItem(0);
+        if (item == null) {
+            return;
+        }
+        final ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName((text == null) ? "" : text);
+        item.setItemMeta(itemMeta);
+        this.setItem(0, item);
+    }
+
+    @Override
+    public void open() {
+        final EntityPlayer player = this.anvil.player;
+        final int count = player.nextContainerCounter();
+
+        player.playerConnection.sendPacket(new PacketPlayOutOpenWindow(count, "minecraft:anvil", new ChatMessage("Anvil")));
+        player.activeContainer = this.anvil;
+        player.activeContainer.windowId = count;
+        player.activeContainer.addSlotListener(player);
+    }
+
+}
---
+++ spigot-parent/v1_8_R3/src/main/java/io/prodity/commons/spigot/legacy/v1_8_R3/ContainerAnvilImpl.java
@@ -0,0 +1,136 @@
+package io.prodity.commons.spigot.legacy.v1_8_R3;
+
+
+import io.prodity.commons.spigot.legacy.gui.anvil.AnvilInputListener;
+import java.util.Collection;
+import java.util.HashSet;
+import net.minecraft.server.v1_8_R3.BlockPosition;
+import net.minecraft.server.v1_8_R3.ContainerAnvil;
+import net.minecraft.server.v1_8_R3.EntityHuman;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import net.minecraft.server.v1_8_R3.ICrafting;
+import net.minecraft.server.v1_8_R3.IInventory;
+import net.minecraft.server.v1_8_R3.InventoryCraftResult;
+import net.minecraft.server.v1_8_R3.InventorySubcontainer;
+import net.minecraft.server.v1_8_R3.ItemStack;
+import net.minecraft.server.v1_8_R3.PlayerInventory;
+import net.minecraft.server.v1_8_R3.Slot;
+import net.minecraft.server.v1_8_R3.World;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftInventory;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftInventoryView;
+
+public class ContainerAnvilImpl extends ContainerAnvil {
+
+    public int a;
+    IInventory resultInventory = new InventoryCraftResult();
+    IInventory craftInventory = new InventorySubcontainer("Repair", true, 2) {
+
+        @Override
+        public void update() {
+            super.update();
+            ContainerAnvilImpl.this.a(this);
+        }
+    };
+    World world;
+    BlockPosition position;
+    String currentName;
+    EntityPlayer player;
+    CraftInventoryView bukkitEntity = null;
+    PlayerInventory playerInventory;
+
+    Collection<AnvilInputListener> listeners = new HashSet<>(3);
+
+    public ContainerAnvilImpl(PlayerInventory playerinventory, World world, BlockPosition blockposition,
+        EntityPlayer player) {
+        super(playerinventory, world, blockposition, player);
+        this.c.clear();
+        this.b.clear();
+        this.playerInventory = playerinventory;
+        this.position = blockposition;
+        this.world = world;
+        this.player = player;
+        this.a(new Slot(this.craftInventory, 0, 27, 47));
+        this.a(new Slot(this.craftInventory, 1, 76, 47));
+        this.a(new Slot(ContainerAnvilImpl.this.resultInventory, 2, 134, 47) {
+
+            @Override
+            public boolean isAllowed(ItemStack itemstack) {
+                return false;
+            }
+
+            @Override
+            public boolean isAllowed(EntityHuman entityhuman) {
+                return false;
+            }
+
+        });
+
+        int i;
+        for (i = 0; i < 3; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                this.a(new Slot(playerinventory, j + (i * 9) + 9, 8 + (j * 18), 84 + (i * 18)));
+            }
+        }
+
+        for (i = 0; i < 9; ++i) {
+            this.a(new Slot(playerinventory, i, 8 + (i * 18), 142));
+        }
+
+    }
+
+    @Override
+    public void a(IInventory iinventory) {
+        super.a(iinventory);
+        if (iinventory == this.craftInventory) {
+            this.e();
+        }
+
+    }
+
+    @Override
+    public void e() {
+    }
+
+    @Override
+    public void addSlotListener(ICrafting icrafting) {
+        super.addSlotListener(icrafting);
+        icrafting.setContainerData(this, 0, this.a);
+    }
+
+    @Override
+    public void b(final EntityHuman entityhuman) {
+    }
+
+    @Override
+    public boolean a(final EntityHuman entityhuman) {
+        return true;
+    }
+
+    @Override
+    public ItemStack b(final EntityHuman entityhuman, final int i) {
+        return null;
+    }
+    
+    public void invalidNameReceived(String name) {
+        this.listeners.forEach(l -> l.onInvalidName(name));
+    }
+
+    @Override
+    public void a(String newName) {
+        final String oldName = this.currentName;
+        this.currentName = newName;
+        this.listeners.forEach((listener) -> listener.onNameChange(oldName, this.currentName));
+    }
+
+    @Override
+    public CraftInventoryView getBukkitView() {
+        if (this.bukkitEntity != null) {
+            return this.bukkitEntity;
+        } else {
+            CraftInventory inventory = new AnvilGuiInventoryImpl(this);
+            this.bukkitEntity = new CraftInventoryView(this.playerInventory.player.getBukkitEntity(), inventory, this);
+            return this.bukkitEntity;
+        }
+    }
+
+}
