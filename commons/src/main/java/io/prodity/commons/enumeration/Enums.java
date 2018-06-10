fa7c59454bb358f64f91fbe315b0954a601fd0a5
FakeNeth <cneth97@gmail.com>
2018-06-10 22:39:24 +0300

    fixes

--- commons/src/main/java/io/prodity/commons/enumeration/Enums.java
+++ commons/src/main/java/io/prodity/commons/enumeration/Enums.java
@@ -1,6 +1,7 @@
 package io.prodity.commons.enumeration;
 
 import com.google.common.base.Preconditions;
+import javax.annotation.Nullable;
 
 public enum Enums {
 
@@ -34,31 +35,41 @@ public enum Enums {
     /**
      * Gets the next constant of the specified enumeration.
      *
-     * @param constant the constant
-     * @return the next constant, or the first constant of the enum if the specified constant is the last of the enumeration
+     * @param constant the constant, or null in which case the returned constant is the first of the enum
+     * @return the next constant, or null if there is no next constant.
      */
-    public static <T extends Enum<T>> T getNextConstant(T constant) {
-        Preconditions.checkNotNull(constant, "constant");
-        final T[] constants = constant.getDeclaringClass().getEnumConstants();
-        Enums.verifyConstantsNotEmpty(constant.getDeclaringClass(), constants);
+    @Nullable
+    public static <T extends Enum<T>> T getNextConstant(Class<T> enumClass, @Nullable T constant) {
+        Preconditions.checkNotNull(enumClass, "enumClass");
+        final T[] constants = enumClass.getEnumConstants();
+        Enums.verifyConstantsNotEmpty(enumClass, constants);
+
+        if (constant == null) {
+            return Enums.getFirstConstant(enumClass);
+        }
 
         final int index = Enums.getConstantIndex(constant);
-        return index == constants.length - 1 ? constants[0] : constants[index + 1];
+        return index == constants.length - 1 ? null : constants[index + 1];
     }
 
     /**
      * Gets the previous constant of the specified enumeration.
      *
-     * @param constant the constant
-     * @return the previous constant, or the last constant of the enum if the specified constant is the first of the enumeration
+     * @param constant the constant, or null in which case the returned constant is the last of the enum
+     * @return the previous constant, or null if there is no previous constant
      */
-    public static <T extends Enum<T>> T getPreviousConstant(T constant) {
-        Preconditions.checkNotNull(constant, "constant");
-        final T[] constants = constant.getDeclaringClass().getEnumConstants();
-        Enums.verifyConstantsNotEmpty(constant.getDeclaringClass(), constants);
+    @Nullable
+    public static <T extends Enum<T>> T getPreviousConstant(Class<T> enumClass, @Nullable T constant) {
+        Preconditions.checkNotNull(enumClass, "enumClass");
+        final T[] constants = enumClass.getEnumConstants();
+        Enums.verifyConstantsNotEmpty(enumClass, constants);
+
+        if (constant == null) {
+            return Enums.getLastConstant(enumClass);
+        }
 
         final int index = Enums.getConstantIndex(constant);
-        return index == 0 ? constants[constants.length - 1] : constants[index - 1];
+        return index == 0 ? null : constants[index - 1];
     }
 
     private static void verifyConstantsNotEmpty(Class<? extends Enum<?>> enumClass, Object[] constants) {
