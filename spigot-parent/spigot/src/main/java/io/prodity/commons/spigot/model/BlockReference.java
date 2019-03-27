3a01e6ada25f49873c285a11416f0ad2dc7e154c
Semx11 <semx11@gmail.com>
2019-03-27 12:09:21 +0100

    add yaw and pitch to LocationReference, add equals and hashcode to models

--- spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/model/BlockReference.java
+++ spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/model/BlockReference.java
@@ -1,53 +1,75 @@
 package io.prodity.commons.spigot.model;
 
+import java.util.Objects;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 
 public class BlockReference {
 
-	public static BlockReference of(Block block) {
-		return BlockReference.of(block.getLocation());
-	}
+    public static BlockReference of(Block block) {
+        return BlockReference.of(block.getLocation());
+    }
 
-	public static BlockReference of(Location location) {
-		return new BlockReference(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
-	}
+    public static BlockReference of(Location location) {
+        return new BlockReference(location.getWorld().getName(), location.getBlockX(),
+                location.getBlockY(), location.getBlockZ());
+    }
 
-	private final String world;
-	private final int x;
-	private final int y;
-	private final int z;
+    private final String world;
+    private final int x;
+    private final int y;
+    private final int z;
 
-	public BlockReference(String world, int x, int y, int z) {
-		this.world = world;
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
+    public BlockReference(String world, int x, int y, int z) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-	public String getWorld() {
-		return this.world;
-	}
+    public String getWorld() {
+        return this.world;
+    }
 
-	public int getX() {
-		return this.x;
-	}
+    public int getX() {
+        return this.x;
+    }
 
-	public int getY() {
-		return this.y;
-	}
+    public int getY() {
+        return this.y;
+    }
 
-	public int getZ() {
-		return this.z;
-	}
+    public int getZ() {
+        return this.z;
+    }
 
-	public Block dereference() {
-		return this.dereferenceLocation().getBlock();
-	}
+    public Block dereference() {
+        return this.dereferenceLocation().getBlock();
+    }
 
-	public Location dereferenceLocation() {
-		return new Location(Bukkit.getWorld(this.world), this.x, this.y, this.z);
-	}
+    public Location dereferenceLocation() {
+        return new Location(Bukkit.getWorld(this.world), this.x, this.y, this.z);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        final BlockReference that = (BlockReference) o;
+        return this.x == that.x &&
+                this.y == that.y &&
+                this.z == that.z &&
+                Objects.equals(this.world, that.world);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.world, this.x, this.y, this.z);
+    }
 
 }
--- spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/model/LocationReference.java
+++ spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/model/LocationReference.java
@@ -1,44 +1,77 @@
 package io.prodity.commons.spigot.model;
 
+import java.util.Objects;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public class LocationReference {
 
-	public static LocationReference of(Location location) {
-		return new LocationReference(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
-	}
+    public static LocationReference of(Location location) {
+        return new LocationReference(location.getWorld().getName(), location.getX(),
+                location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+    }
 
-	private final String world;
-	private final double x;
-	private final double y;
-	private final double z;
+    private final String world;
+    private final double x;
+    private final double y;
+    private final double z;
+    private final float yaw;
+    private final float pitch;
 
-	public LocationReference(String world, double x, double y, double z) {
-		this.world = world;
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
+    public LocationReference(String world, double x, double y, double z) {
+        this(world, x, y, z, 0, 0);
+    }
 
-	public String getWorld() {
-		return this.world;
-	}
+    public LocationReference(String world, double x, double y, double z, float yaw, float pitch) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
 
-	public double getX() {
-		return this.x;
-	}
+    public String getWorld() {
+        return this.world;
+    }
 
-	public double getY() {
-		return this.y;
-	}
+    public double getX() {
+        return this.x;
+    }
 
-	public double getZ() {
-		return this.z;
-	}
+    public double getY() {
+        return this.y;
+    }
 
-	public Location dereference() {
-		return new Location(Bukkit.getWorld(this.world), this.x, this.y, this.z);
-	}
+    public double getZ() {
+        return this.z;
+    }
+
+    public Location dereference() {
+        return new Location(Bukkit.getWorld(this.world), this.x, this.y, this.z, this.yaw,
+                this.pitch);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        final LocationReference that = (LocationReference) o;
+        return Double.compare(that.x, this.x) == 0 &&
+                Double.compare(that.y, this.y) == 0 &&
+                Double.compare(that.z, this.z) == 0 &&
+                Float.compare(that.yaw, this.yaw) == 0 &&
+                Float.compare(that.pitch, this.pitch) == 0 &&
+                Objects.equals(this.world, that.world);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.world, this.x, this.y, this.z, this.yaw, this.pitch);
+    }
 
 }
