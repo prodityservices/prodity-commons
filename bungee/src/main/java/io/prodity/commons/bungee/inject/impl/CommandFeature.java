49d486be6c843f9bf4b8a9b99f41a75709f1117e
Admin <tehneon1212@gmail.com>
2019-12-08 17:35:54 -0500

    CommandManagers are now properly binded

--- bungee/src/main/java/io/prodity/commons/bungee/inject/impl/CommandFeature.java
+++ bungee/src/main/java/io/prodity/commons/bungee/inject/impl/CommandFeature.java
@@ -1,6 +1,7 @@
 package io.prodity.commons.bungee.inject.impl;
 
 import co.aikar.commands.BungeeCommandManager;
+import co.aikar.commands.CommandManager;
 import io.prodity.commons.bungee.inject.CommandManagerCustomizer;
 import io.prodity.commons.bungee.plugin.ProdityBungeePlugin;
 import io.prodity.commons.command.ProdityCommand;
@@ -13,23 +14,40 @@ import java.util.List;
 public class CommandFeature implements InjectionFeature {
 
 	@Override
-	public void onEnable(ProdityPlugin plugin) {
-		ProdityBungeePlugin spigotPlugin = (ProdityBungeePlugin) plugin;
-		List<ProdityCommand> commands = InjectUtils.getLocalServices(plugin, ProdityCommand.class);
+	public void preEnable(ProdityPlugin plugin) {
+		final ProdityBungeePlugin spigotPlugin = (ProdityBungeePlugin) plugin;
 
+		final BungeeCommandManager commandManager = new BungeeCommandManager(spigotPlugin);
+		commandManager.enableUnstableAPI("help");
+
+		// bind the command manager prior to loading the commands in-case any commands inject a service
+		// that require the command manager as commands are treated as Eager
+		this.bind(plugin, binder -> {
+			binder.bind(commandManager)
+					.named(plugin.getName())
+					.to(commandManager.getClass())
+					.to(CommandManager.class);
+		});
+
+		final List<ProdityCommand> commands = InjectUtils.getLocalServices(plugin, ProdityCommand.class);
 		if (!commands.isEmpty()) {
-			BungeeCommandManager manager = new BungeeCommandManager(spigotPlugin);
-			manager.enableUnstableAPI("help");
-			List<CommandManagerCustomizer> customizers = InjectUtils.getDependentServices(plugin, CommandManagerCustomizer.class);
+			final List<CommandManagerCustomizer> customizers = InjectUtils.getDependentServices(plugin, CommandManagerCustomizer.class);
 			for (CommandManagerCustomizer customizer : customizers) {
-				customizer.customize(manager);
+				customizer.customize(commandManager);
 			}
+
 			for (ProdityCommand command : commands) {
-				manager.registerCommand(command);
+				commandManager.registerCommand(command);
 			}
-			this.bind(plugin, binder -> {
-				binder.bind(manager);
-			});
 		}
 	}
+
+	@Override
+	public void preDisable(ProdityPlugin plugin) {
+		final BungeeCommandManager commandManager = plugin.getServices().getService(BungeeCommandManager.class);
+		if (commandManager != null) {
+			commandManager.unregisterCommands();
+		}
+	}
+
 }
--- spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/inject/impl/CommandFeature.java
+++ spigot-parent/spigot/src/main/java/io/prodity/commons/spigot/inject/impl/CommandFeature.java
@@ -1,34 +1,53 @@
 package io.prodity.commons.spigot.inject.impl;
 
 import co.aikar.commands.BukkitCommandManager;
+import co.aikar.commands.CommandManager;
 import io.prodity.commons.command.ProdityCommand;
 import io.prodity.commons.inject.InjectionFeature;
 import io.prodity.commons.inject.impl.InjectUtils;
 import io.prodity.commons.plugin.ProdityPlugin;
 import io.prodity.commons.spigot.inject.CommandManagerCustomizer;
 import io.prodity.commons.spigot.inject.SpigotInjectedPlugin;
+
 import java.util.List;
 
 public class CommandFeature implements InjectionFeature {
 
-    @Override
-    public void onEnable(ProdityPlugin plugin) {
-        SpigotInjectedPlugin spigotPlugin = (SpigotInjectedPlugin) plugin;
-        List<ProdityCommand> commands = InjectUtils.getLocalServices(plugin, ProdityCommand.class);
+	@Override
+	public void preEnable(ProdityPlugin plugin) {
+		final SpigotInjectedPlugin spigotPlugin = (SpigotInjectedPlugin) plugin;
 
-        if (!commands.isEmpty()) {
-            BukkitCommandManager manager = new BukkitCommandManager(spigotPlugin);
-            manager.enableUnstableAPI("help");
-            List<CommandManagerCustomizer> customizers = InjectUtils.getDependentServices(plugin, CommandManagerCustomizer.class);
-            for (CommandManagerCustomizer customizer : customizers) {
-                customizer.customize(manager);
-            }
-            for (ProdityCommand command : commands) {
-                manager.registerCommand(command);
-            }
-            this.bind(plugin, binder -> {
-                binder.bind(manager);
-            });
-        }
-    }
+		final BukkitCommandManager commandManager = new BukkitCommandManager(spigotPlugin);
+		commandManager.enableUnstableAPI("help");
+
+		// bind the command manager prior to loading the commands in-case any commands inject a service
+		// that require the command manager as commands are treated as Eager
+		this.bind(plugin, binder -> {
+			binder.bind(commandManager)
+					.named(plugin.getName())
+					.to(commandManager.getClass())
+					.to(CommandManager.class);
+		});
+
+		final List<ProdityCommand> commands = InjectUtils.getLocalServices(plugin, ProdityCommand.class);
+		if (!commands.isEmpty()) {
+			final List<CommandManagerCustomizer> customizers = InjectUtils.getDependentServices(plugin, CommandManagerCustomizer.class);
+			for (CommandManagerCustomizer customizer : customizers) {
+				customizer.customize(commandManager);
+			}
+
+			for (ProdityCommand command : commands) {
+				commandManager.registerCommand(command);
+			}
+		}
+	}
+
+	@Override
+	public void preDisable(ProdityPlugin plugin) {
+		final BukkitCommandManager commandManager = plugin.getServices().getService(BukkitCommandManager.class);
+		if (commandManager != null) {
+			commandManager.unregisterCommands();
+		}
+	}
+
 }
